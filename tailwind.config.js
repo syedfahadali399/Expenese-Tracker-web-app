2e1e32915913591117a505b4f16542847185b548
Syed Fahad <syedf8707@gmail.com>
2026-05-16 23:20:01 +0500

    initial push

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    screens: {
+      'xs': '480px',
+    'max-xs': { 'max': '479px' },
+
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    }
+  }
+}
